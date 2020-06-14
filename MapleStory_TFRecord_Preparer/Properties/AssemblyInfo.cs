d94a76a50768b3a651bfabccdc8c4ee3d03f059d
charlescao460 <charlescao@csr460.com>
2020-06-14 14:19:57 -0400

    Add commandline parser

---
+++ MapleStory_TFRecord_Preparer/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MapleStory_TFRecord_Preparer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GITHUB.COM/CHARLESCAO460")]
+[assembly: AssemblyProduct("MapleStory_TFRecord_Preparer")]
+[assembly: AssemblyCopyright("Copyright CHARLESCAO460 © 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5b0e37ac-c560-4fc9-8388-4c8d1a1f4806")]
+
