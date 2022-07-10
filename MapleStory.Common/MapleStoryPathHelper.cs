2f5f3302a6b4191d5b7b3fe434be3f6118d2acbc
charlescao460 <charlescao@csr460.com>
2022-07-10 02:23:13 -0400

    Implement maplestory discovery with GMS

--- MapleStory.Common/MapleStoryPathHelper.cs
+++ MapleStory.Common/MapleStoryPathHelper.cs
@@ -13,27 +13,43 @@ namespace MapleStory.Common
     /// </summary>
     public static class MapleStoryPathHelper
     {
-        private static readonly string REG_KEY_PATH =
+        private static readonly string REG_KEY_PATH_CMS =
             @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\MapleStory"; // Tested for CMS
+        private static readonly string REG_VALUE_NAME_CMS = @"UninstallString";
 
-        private static readonly string REG_VALUE_NAME = @"UninstallString";
+        private static readonly string REG_KEY_PATH_GMS = @"SOFTWARE\WOW6432Node\Wizet\MapleStory";
+        private static readonly string REG_VALUE_NAME_GMS = @"ExecPath";
 
         public static readonly string MapleStoryExecutableName = @"MapleStory.exe";
 
-        public static readonly string MapleStoryBaseWzName = @"Base.wz";
+        public static readonly string MapleStoryBaseWzName = @"Data\Base\Base.wz";
 
         private static readonly Lazy<string> _mapleStoryInstallDirectory = new Lazy<string>(() =>
         {
+            // For CMS
             try
             {
-                RegistryKey key = Registry.LocalMachine.OpenSubKey(REG_KEY_PATH); // Possible null
-                string uninstallPath = key.GetValue(REG_VALUE_NAME) as string; // Possible invalid cast
+                RegistryKey key = Registry.LocalMachine.OpenSubKey(REG_KEY_PATH_CMS); // Possible null
+                string uninstallPath = key.GetValue(REG_VALUE_NAME_CMS) as string; // Possible invalid cast
                 return Path.GetDirectoryName(uninstallPath); // Possible corrupted path
             }
             catch (Exception ex) when (ex is NullReferenceException || ex is InvalidCastException || ex is ArgumentException)
             {
-                return string.Empty;
+                // ignored, not found
             }
+
+            // For GMS
+            try
+            {
+                RegistryKey key = Registry.LocalMachine.OpenSubKey(REG_KEY_PATH_GMS); // Possible null
+                string execPath = key.GetValue(REG_VALUE_NAME_GMS) as string; // Possible invalid cast
+                return execPath; 
+            }
+            catch (Exception ex) when (ex is NullReferenceException || ex is InvalidCastException)
+            {
+                // ignored, not found
+            }
+            return string.Empty;
         });
 
         /// <summary>
